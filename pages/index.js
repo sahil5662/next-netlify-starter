b14214b88dd0abb96a6a96cd5c756d1199dc5ede
sahil5662 <sahildhullyt@gmail.com>
2025-01-15 14:50:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,10 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
+    
+    <script src="https://cdn.botpress.cloud/webchat/v2.2/inject.js"></script>
+<script src="https://files.bpcontent.cloud/2025/01/13/08/20250113085806-9CV2TKBR.js"></script>
+    
 
       <Footer />
     </div>
